869141df3645ea19efa7836bcb338203da04b589
Kaue <kauezinho_naruto@hotmail.com>
2023-09-05 16:34:49 -0300

    Adicionando mais produtos

--- src/routes/Produtos/listaProdutos.js
+++ src/routes/Produtos/listaProdutos.js
@@ -10,8 +10,24 @@ export const listaProdutos = [
         preco :349.87
     },
     {
-        id:3,nome:"Monitor",
+        id: 3,
+        nome:"Monitor Comum",
         preco:699.99
+    },
+    {
+        id: 4,
+        nome: 'Gabinete',
+        preco: 150
+    },
+    {
+        id: 5 ,
+        nome:'Monitor Gamer' ,
+        preco :2499.00
+    },
+    {
+        id:6,
+        nome:"Placa de video",
+        preco:2600.00
     },
     
 ]
