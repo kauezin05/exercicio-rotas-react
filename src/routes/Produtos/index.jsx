5bf31269266b6b516a5dfa599e3f1feeb2b1f94e
kauezin05 <kauezinho_naruto@hotmail.com>
2023-09-05 21:32:07 -0300

    Fim

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -7,7 +7,7 @@ export default function Produtos(){
             <h1>Produtos</h1>
             {listaProdutos.map(prod => (
                 <div key={prod.id}>
-                    <Link to={`/produtos/editar/${prod.id}`}>Editar o produto: {prod.nome}</Link>
+                    <Link to={`/editar/produtos/${prod.id}`}>Editar o produto: {prod.nome}</Link>
                 </div>
             ))
 
